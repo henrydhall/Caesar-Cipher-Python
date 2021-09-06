1eb234cfa048ec8aae0fb78828ced172019a8701
HenrydHall <henrydhall3.14@gmail.com>
2021-09-06 11:02:09 -0600

    Made major changes.

--- CaesarCipher.py
+++ CaesarCipher.py
@@ -10,6 +10,12 @@ import string, collections, sys
 from pathlib import Path
 
 def caesarEncrypt( unencryptedString, rotateNum ):
+    """
+    # A simple Caesar Cipher is implemented with a 100 character alphabet.
+    # It inputs the string that you want encrypted, and the rotation you want performed on it.
+    # It returns an encrypted string.
+    # This is where the magic happens.
+    """
     #Create deques of characters.
     charactersDeque = collections.deque( string.printable )
     rotatedCharactersDeque = collections.deque( string.printable )
@@ -25,12 +31,44 @@ def caesarEncrypt( unencryptedString, rotateNum ):
 def readFromFile(fileName):
     """
     # This reads text from a file and returns contents as a string.
+    # Returns "" if no file found.
     """
-    fileToRead = Path(fileName)
-    return fileToRead.read_text()
+    try:
+        fileToRead = Path(fileName)
+        fileContents = fileToRead.read_text()
+        return fileContents
+    except FileNotFoundError:
+        print(f"FileNotFoundError: {fileName} doesn't exist.")
+        return ""
+
+def writeToFile(fileName, writeString):
+    """
+    # Writes writeString to fileName.
+    # Returns nothing.
+    """
+    fileToWrite = Path(fileName)
+    fileToWrite.write_text(writeString)
+
+def caesarDecrypt(encryptedString, rotateNum):
+    """
+    # Accepts an encrypted string, and the rotation that was used to encrypt it.
+    # Returns the decrypted string.
+    """
+    #Create deques of characters.
+    charactersDeque = collections.deque( string.printable )
+    rotatedCharactersDeque = collections.deque( string.printable )
+    rotatedCharactersDeque.rotate(-rotateNum)
+    #Make the deques strings for the translations.
+    charactersString = "".join(list(charactersDeque))
+    rotatedCharactersString = "".join(list(rotatedCharactersDeque))
+    #Create translation table and perform the translation.
+    translateTable = encryptedString.maketrans( rotatedCharactersString, charactersString )
+    decryptedString = encryptedString.translate( translateTable )
+    return decryptedString
 
 if __name__ == "__main__":
     print( sys.argv ) #Temporary to keep track of command line input
+    #TODO: put this all in another file.
     if len(sys.argv) < 3:
         print("Usage: python CaesarEncrypter.py [\"text to encrypt\"] [rotation]")
         print("Usage: python CaesarEncrypter.py -i [file to encrypt] [rotation]")
